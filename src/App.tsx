808e3fe5a7ae5a4fc77d90819fd083010786e1f3
oscar19023 <oscar@ocm-media.com>
2026-05-10 16:18:55 +0200

    Fix typo in navigation logo text

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ export default function App() {
 function Nav() {
   return (
     <nav className="nav">
-      <div className="nav-logo">Queckfeldt & Jonsén Kings 123</div>
+      <div className="nav-logo">Queckfeldt & Jonsén King</div>
       <ul className="nav-links">
         <li><a href="#about">About</a></li>
         <li><a href="#projects">Projects</a></li>
